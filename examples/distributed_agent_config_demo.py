9bbe48aaf1b2adb4cf9fd98e4c27932735637e68
Eugen <745045993@qq.com>
2025-03-25 21:54:36 +0800

    remove grpcio-tools

--- examples/distributed_agent_config_demo.py
+++ examples/distributed_agent_config_demo.py
@@ -1,14 +1,19 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../isek")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../examples")))
 import time
 from isek.isek_config import IsekConfig
 
 
 def main():
     
-    sylana_config = IsekConfig("examples/sylana/sylana_config.yaml")
+    sylana_config = IsekConfig("sylana/sylana_config.yaml")
     sylana_agent = sylana_config.load_agent()
     sylana_agent.build(daemon=True)
 
-    dobby_config = IsekConfig("examples/dobby/dobby_config.yaml")
+    dobby_config = IsekConfig("dobby/dobby_config.yaml")
     dobby_agent = dobby_config.load_agent()
     dobby_agent.build(daemon=True)
 
