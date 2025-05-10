592b7dee9f923c8494347ceb6744f6e9fd642700
Eugen <745045993@qq.com>
2025-05-10 14:22:51 +0800

    add ci

--- tests/log_test.py
+++ tests/log_test.py
@@ -1,12 +1,10 @@
 from isek.util.logger import LoggerManager, logger
 
-def build(debug=False):
+
+def test_build(debug=False):
     LoggerManager.init(debug=debug)
 
     logger.info("Logger initialized with debug mode: {}".format(debug))
     logger.debug("This is a debug message")
     logger.info("This is an info message")
-
-
-if __name__ == "__main__":
-    build(debug=True)
+    return True
