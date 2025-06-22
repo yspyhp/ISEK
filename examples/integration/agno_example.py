8b67b57362a38b6fb87e567f7b05f5582b3630cb
Eugen <745045993@qq.com>
2025-06-23 00:22:22 +0800

    merge

--- examples/integration/agno_example.py
+++ examples/integration/agno_example.py
@@ -1,26 +1,40 @@
-from isek.team import Team, TeamCard
+from agno.agent import Agent
+from agno.models.deepseek import DeepSeek
+
+from isek.adapter.base import Adapter, AdapterCard
 from isek.node.node_v2 import Node
 
-from agno.agent import Agent
-from agno.models.openai import OpenAIChat
-from isek.team.one_man_team import OneManTeam
 
+class RandomNumberAdapter(Adapter):
 
-random_agent = Agent(
-        model=OpenAIChat(id="gpt-4o"),
-        tools=[],
-        instructions=[
-            "Only can generator a random number"
-        ],
-        markdown=True,
-    )
+    def __init__(self):
+        self.random_agent = Agent(
+            model=DeepSeek(api_key="sk-2d52100fe9b348afa71dc9d5b31db07f"),
+            tools=[],
+            instructions=[
+                "Only can generator a random number"
+            ],
+            markdown=True,
+        )
 
-rn_team = OneManTeam(name="RandomNumberTeam", description="A team that generates random numbers", agent=random_agent)
+    def run(self, prompt: str) -> str:
+        output_msg = self.random_agent.run(prompt)
+        return output_msg.content
+
+    def get_adapter_card(self) -> AdapterCard:
+        return AdapterCard(
+            name="Random Number Generator",
+            bio="",
+            lore="",
+            knowledge="",
+            routine="",
+        )
+
 
 # Create the server node.
-server_node = Node(node_id="RN", port=8080, team=rn_team)
+server_node = Node(node_id="RN", port=8080, adapter=RandomNumberAdapter())
 
 # Start the server in the foreground.
 server_node.build_server(daemon=True)
-print(server_node.team.run("random a number 0-10"))
+print(server_node.adapter.run("random a number 0-10"))
 
